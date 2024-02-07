8271dd32c3160c2e8af47829e9d89372f4f8f043
MunshiSarifulIslam <munshsarif176@gmail.com>
2024-02-07 16:09:15 +0530

    Initial Commit

---
+++ MunshiTestProject/MunshiTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MunshiTestProjectApp.swift
+//  MunshiTestProject
+//
+//  Created by User on 07/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MunshiTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
